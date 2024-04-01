33e613786c0705bfc388584ec1ae836119aeefdb
samranvirk44 <samranvirk44@gmail.com>
2024-04-01 10:44:55 +0800

    set_url_prediction_app

--- crypto/urls.py
+++ crypto/urls.py
@@ -15,17 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
     path('getAllCryptoNames', views.getAllCryptoNames, name='getAllCryptoNames'),
     path('currentValues', views.currentValues, name='currentValues'),
     path('getBTCValue', views.getBTCValue, name='getBTCValue'),
-
-
+    path("prediction", include("prediction.urls"))
 
 ]
+
+
+
+
+
