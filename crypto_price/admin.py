929bdb18cd0a382908ae7b6b2f6a3aa3a171f096
hammadjoshi <mianhammad768@gmail.com>
2024-05-22 14:58:16 +0800

    two line graph

--- crypto_price/admin.py
+++ crypto_price/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from . import models
 admin.site.register(models.DailyCryptoData)
 admin.site.register(models.MinuteCryptoData)
+admin.site.register(models.prediction_data)
 
 # Register your models here.
--- crypto_price/urls.py
+++ crypto_price/urls.py
@@ -6,8 +6,7 @@ urlpatterns = [
     path("DailyDataView/",views.DailyDataView.as_view(),name="DailyDataView"),
     path("MinuteDataView/",views.MinuteDataView.as_view(),name="MinuteDataView"),
     path("PredictionView/",views.PredictionView.as_view(),name="PredictionView"),
-    path("createDailyData/", views.createDailyData, name="CreateDailyData"),
-    path("createMinuteData/", views.createMinuteData, name="createMinuteData"),
-
+    
+    
     # path("admin/", admin.site.urls),
 ]
